66e40819fc5c73143e670f197074b93f2aabbe95
ccarlsten73 <caroline.carlsten@gmail.com>
2026-05-04 14:21:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.functions import (col)
 import requests
 import pandas as pd
 
-# st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
 # Write directly to the app.
 st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
