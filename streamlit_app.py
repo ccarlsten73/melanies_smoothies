8508263ef248ea278e1abcaadc187054ad2d1c25
ccarlsten73 <caroline.carlsten@gmail.com>
2026-05-04 13:10:43 +0200

    Update streamlit_app.py
    Added smoothiefroot part

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages.
 import streamlit as st
 from snowflake.snowpark.functions import (col)
+import requests  
 
 st.set_page_config(layout="wide")
 
@@ -47,3 +48,6 @@ if ingredients_list:
             session.sql(my_insert_stmt).collect()
             
             st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
